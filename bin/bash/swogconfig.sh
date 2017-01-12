d19a291c6401e82284a62eb7c1eb8993d6cc178a
Davide Pellegrino <davidepellegrino@iMac-di-Davide.local>
2017-01-12 15:41:49 +0100

    aggiornamento 1.3.7 BASH INSTALLING

--- bin/bash/swogconfig.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-SWOG_COPY=( "package.json" "gruntfile.js" "grunt" "scss" "js" )
--- bin/bash/swoginstall.sh
+++ bin/bash/swoginstall.sh
@@ -1,37 +1,41 @@
 #!/bin/bash
-
 # **********************
-# Installazione di SWOG con bootstrap 3
+# Installazione di SWOG
 # **********************
 set -e # Exit script on error
-# inlcudo il file di config che trova a destinazione
-if [ -f ./bin/bash/config.sh ];then
-  source ./bin/bash/config.sh
+# SWOG è installato in vendor/fvoxlab/swog
+VENDORDIR='vendor/fvoxlab/swog'
+BASEDIR=$1
+# i file da copiare nella root del progetto
+SWOG_COPY=( "package.json" "gruntfile.js" "grunt" "scss" "js" )
+# if [ -f ./bin/bash/swogconfig.sh ];then
+#   source ./bin/bash/swogconfig.sh
+# fi
+if [ -z $BASEDIR ]; then
+   echo "SWOG base dir is Empty"
+   exit
 fi
-if [ -f ./bin/bash/swogconfig.sh ];then
-  source ./bin/bash/swogconfig.sh
-fi
-
-
-# SE E SOLO SE, NON TROVO i file di SWOG COPY
-if [ ! -f 'package.json' ];then
+# SE E SOLO SE, NON TROVO i file di SWOG COPY nella root del progetto
+if [ ! -f "$BASEDIR/package.json" ];then
   echo "SWOG installing .."
   for fsobject in "${SWOG_COPY[@]}";do
-    if cp -R "$DIRSWOG/$fsobject" $PROJECTDIR;then
-      echo "$DIRSWOG/$fsobject OK"
+    if cp -R "$VENDORDIR/$fsobject" $BASEDIR;then
+      echo "$fsobject OK"
     else
-      echo "$DIRSWOG/$fsobject FAILED!"
+      echo "$fsobject FAILED!"
     fi
   done
   sleep 1
+else
+  echo "SWOG is installed"
 fi
 
-# Run Grunt tasks
-if [ ! -d 'node_modules' ];then
-  echo "NPM installing ..."
-  npm install --quiet --silent
-  # sleep 1
-else
-  echo "Updating NPM ..."
-  #rm -rf node_modules
-fi
+# # Run Grunt tasks
+# if [ ! -d 'node_modules' ];then
+#   echo "NPM installing ..."
+#   npm install --quiet --silent
+#   # sleep 1
+# else
+#   echo "Updating NPM ..."
+#   #rm -rf node_modules
+# fi
