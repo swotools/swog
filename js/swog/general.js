d04c9bf1a2d32d85b9d71f33ab467cd448c75d2e
AlfaPapa <papa@alfa.com>
2018-01-16 10:52:14 +0100

    Funzione base di logout

--- js/swog/general.js
+++ js/swog/general.js
@@ -1,8 +1,12 @@
-
 (function() {
 	'use strict';
 
 	$('[data-toggle="tooltip"]').tooltip();
-	$('[data-toggle="popover"]').popover();
+	//$('[data-toggle="popover"]').popover();
+
+	$('#logoutlink').click(function() {
+		$('#logout').submit();
+		return false;
+	});
 
 })();
